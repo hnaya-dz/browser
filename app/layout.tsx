8ecdca2e9fbd7db8d7370ccb4746b98555137d0a
IBFTS <nacib.hamida@ibfts.com>
2026-06-05 04:50:45 +0100

    Refactor RootLayout to include TabPositionProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import TabBar from "@/components/tabbar";
 import { TabProvider } from "@/context/tabcontext";
 import { LoadingProvider } from "@/context/loadingcontext";
 import { LanguageProvider } from "@/context/langcontext";
+import { TabPositionProvider } from "@/context/tabpositioncontext";
 import URLBar from "@/components/urlbar";
 import HtmlWrapper from "@/components/html-wrapper";
 
@@ -23,8 +24,8 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
+    { media: "(prefers-color-scheme: light)", color: "#001a0e" },
+    { media: "(prefers-color-scheme: dark)", color: "#001a0e" },
   ],
 };
 
@@ -32,23 +33,25 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html suppressHydrationWarning lang="ar" dir="rtl">
       <head />
-      <body className={clsx("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+      <body className={clsx("min-h-screen font-sans antialiased", fontSans.variable)}>
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <LanguageProvider>
-            <HtmlWrapper>
-              <div className="relative flex flex-col">
-                <main className="container">
-                  <LoadingProvider>
-                    <TabProvider>
-                      <TabBar />
-                      <Navbar />
-                      <URLBar />
-                      {children}
-                    </TabProvider>
-                  </LoadingProvider>
-                </main>
-              </div>
-            </HtmlWrapper>
+            <TabPositionProvider>
+              <HtmlWrapper>
+                <div className="relative flex flex-col">
+                  <main className="container">
+                    <LoadingProvider>
+                      <TabProvider>
+                        <TabBar />
+                        <Navbar />
+                        <URLBar />
+                        {children}
+                      </TabProvider>
+                    </LoadingProvider>
+                  </main>
+                </div>
+              </HtmlWrapper>
+            </TabPositionProvider>
           </LanguageProvider>
         </Providers>
       </body>
