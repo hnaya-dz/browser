6bc4c7ba4152570ef8b0ebe7da0a18ca6964883d
IBFTS <nacib.hamida@ibfts.com>
2026-06-07 14:36:13 +0100

    Refactor IPC methods in preload.js

--- public/preload.js
+++ public/preload.js
@@ -1,12 +1,20 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
+  // Envoi unidirectionnel (fire & forget)
   send: (channel, data) => {
-    console.log(`Sending to ${channel} with args:`, data);
-    ipcRenderer.send(channel, data)
+    ipcRenderer.send(channel, data);
   },
-  receive: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
+  // Écoute des événements du main process
+  receive: (channel, func) => {
+    ipcRenderer.on(channel, (event, ...args) => func(...args));
+  },
+  // Suppression d'un listener
   removeListener: (channel, func) => {
     ipcRenderer.removeListener(channel, func);
-  }
+  },
+  // Appel bidirectionnel avec réponse (Promise) — utilisé pour yt-dlp
+  invoke: (channel, ...args) => {
+    return ipcRenderer.invoke(channel, ...args);
+  },
 });
