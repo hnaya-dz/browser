d3377cdd682014613a24a8e508eb1dd7d8ea273d
IBFTS <nacib.hamida@ibfts.com>
2026-06-07 20:21:09 +0100

    Open developer tools in non-packaged mode
    Added a line to open developer tools for diagnostics.

--- public/electron.js
+++ public/electron.js
@@ -59,8 +59,9 @@ const createWindow = () => {
   if (app.isPackaged) {
     appServe(mainWindow).then(() => mainWindow.loadURL("app://index.html"));
   } else {
-    mainWindow.loadURL("http://localhost:3000");
-    mainWindow.webContents.on("did-fail-load", () => mainWindow.webContents.reloadIgnoringCache());
+   mainWindow.loadURL("http://localhost:3000");
+mainWindow.webContents.on("did-fail-load", () => mainWindow.webContents.reloadIgnoringCache());
+mainWindow.webContents.openDevTools(); // ← TEMPORAIRE pour diagnostic
   }
   mainWindow.on("closed", () => { mainWindow = null; });
 };
