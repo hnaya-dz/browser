59da8b47d878fe146c6ad45e47a9349d02a46420
IBFTS <nacib.hamida@ibfts.com>
2026-06-19 20:51:10 +0100

    Enhance next.config.js for static export and ESLint
    Added ESLint configuration to ignore warnings during production builds and enabled unoptimized images for static export.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Enables static export
+  output: "export", // Export statique pour Electron
+  eslint: {
+    // Les warnings ESLint n'empêchent pas le build de production
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    // Nécessaire pour l'export statique
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
