92e8c7ceea9f33eacc579481f31d96601c10f945
IBFTS <nacib.hamida@ibfts.com>
2026-06-25 02:45:38 +0100

    Disable DevTools in production builds
    Removed the conditional opening of DevTools for development.

--- public/electron.js
+++ public/electron.js
@@ -47,10 +47,7 @@ mainWindow = new BrowserWindow({
   const chromeUA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36";
   mainWindow.webContents.setUserAgent(chromeUA);
 
-  // ✅ PATCH 8 — DevTools uniquement en développement
-  if (!app.isPackaged) {
-    mainWindow.webContents.openDevTools();
-  }
+// DevTools désactivés
 
   if (app.isPackaged) {
     appServe(mainWindow).then(() => mainWindow.loadURL("app://index.html"));
