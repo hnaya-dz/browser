1b3b57e5396d09ddc297881a2292234f70d61d21
Nacib <nacib.hamida@ibfts.com>
2026-06-28 01:17:01 +0100

    Add '--no-update-check' option to ytDlp spawn

--- public/electron.js
+++ public/electron.js
@@ -144,6 +144,7 @@ ipcMain.handle("get-video-info", async (event, url) => {
     let output = "";
     let errOutput = "";
     const proc = spawn(ytDlpPath, [
+      "--no-update-check",
       "--dump-json",
       "--no-playlist",
       "--socket-timeout", "15",
