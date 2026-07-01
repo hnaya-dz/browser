7b24a73107814bc8a2ee6d517c4a3bfde6ece723
Nacib <nacib.hamida@ibfts.com>
2026-07-01 19:26:08 +0100

    Refactor DevTools opening condition

--- public/electron.js
+++ public/electron.js
@@ -108,10 +108,10 @@ mainWindow = new BrowserWindow({
   const chromeUA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36";
   mainWindow.webContents.setUserAgent(chromeUA);
 
-// ✅ PATCH 8 — DevTools uniquement en développement
-if (!app.isPackaged) {
-  mainWindow.webContents.openDevTools();
-}
+// DevTools uniquement en développement
+  if (!app.isPackaged) {
+    mainWindow.webContents.openDevTools();
+  }
 
   if (app.isPackaged) {
     appServe(mainWindow).then(() => mainWindow.loadURL("app://index.html"));
